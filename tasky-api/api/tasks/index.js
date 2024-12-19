6458c561b6f6f456c4b1534b92a82e55ffe3ea4e
unknown <944325009@qq.com>
2024-12-19 01:23:49 +0000

    added routing for posting a new task

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { tasksData } from './tasksData';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router(); 
 
@@ -16,5 +18,22 @@ router.get('/:id', (req, res) => {
     }
     return res.status(200).json(task);
 });
+
+//Add a task
+router.post('/', (req, res) => {
+    const { title, description, deadline, priority, done } = req.body;
+    const newTask = {
+        id: uuidv4(),
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask);
+    res.status(201).json(newTask);
+    tasksData.total_results++;
+});
+
 
 export default router;
