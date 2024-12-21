bcdfd879d46ca2f9965d1835b718f5197fc7d04f
unknown <944325009@qq.com>
2024-12-21 20:41:59 +0000

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,16 +3,18 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
-import defaultErrHandler from './errHandler'
+import defaultErrHandler from './errHandler';
+import moviesRouter from './api/movies';   //import movies router
 
 dotenv.config();
 
-const app = express();
+const app = express(); 
 const port = process.env.PORT; 
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
