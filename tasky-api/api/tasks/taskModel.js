070a7cd233206a0f6b2e9444fde0d154b2996a14
unknown <944325009@qq.com>
2024-12-21 00:13:56 +0000

    Mongo lab; Included Mongoose

---
+++ tasky-api/api/tasks/taskModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+  title:  String,
+  description:  String ,
+  deadline: Date,
+    done: Boolean,
+    priority: {type: String, enum: ["Low","Medium","High"]},
+    created_at: Date,
+    updated_at: Date
+});
+
+export default mongoose.model('Task', TaskSchema);
+
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import './db';
+
 
 dotenv.config();
 
